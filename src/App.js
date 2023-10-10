3098e81bb382300c29506464f3029e4ab162499e
Vitalii MV <agronom9913@gmail.com>
2023-10-10 19:29:24 +0300

    2023.10.10 filter

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Items from "./components/Items";
+import Categories from "./components/Categories";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
@@ -39,7 +41,7 @@ class App extends React.Component {
           title: "Лампа",
           img: "wall-light.jpeg",
           desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-          category: "sofa",
+          category: "light",
           price: "24.99",
         },
         {
@@ -47,37 +49,49 @@ class App extends React.Component {
           title: "Стілець білий",
           img: "chair-white.jpeg",
           desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-          category: "sofa",
+          category: "chairs",
           price: "24.99",
         },
       ],
     };
+    this.state.currentItems = this.state.items;
     this.addToOrder = this.addToOrder.bind(this);
     this.deleteOrder = this.deleteOrder.bind(this);
-
+    this.chooseCategory = this.chooseCategory.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
-        <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
-        <Items items={this.state.items} onAdd={this.addToOrder} />
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items items={this.state.currentItems} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
   }
 
-  deleteOrder(id){
-     this.setState({orders:this.state.orders.filter(el=>el.id !==id)})
+  chooseCategory(category) {
+    if (category === "all") {
+      this.setState({ currentItems: this.state.items });
+      return;
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter((el) => el.category === category),
+    });
   }
 
-  addToOrder(item) {  
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) });
+  }
+
+  addToOrder(item) {
     // для того щоб не повторювались товари у корзині
     let isInArray = false;
     this.state.orders.forEach((el) => {
       if (el.id === item.id) isInArray = true;
     });
-    if(!isInArray)
-    this.setState({ orders: [...this.state.orders, item] });
+    if (!isInArray) this.setState({ orders: [...this.state.orders, item] });
   }
 }
 

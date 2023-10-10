211dcb2ea09f724939061e614eafc2834433162a
Vitalii MV <agronom9913@gmail.com>
2023-10-10 14:25:29 +0300

    2023.10.10 korzina

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaShopify } from "react-icons/fa";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
       <div>
         <span className="logo">House Staff</span>
         <ul className="nav">
           <li> Про нас</li>
-          <li>Контакти</li>
-          <li>Кабінет</li>
+          <li> Контакти</li>
+          <li> Кабінет</li>
         </ul>
+        <FaShopify
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop-cart-button ${cartOpen && "active"}`}
+        />
+        {cartOpen && (
+          <div className="shop-cart">
+            <h3>111</h3>
+          </div>
+        )}
       </div>
       <div className="presentation"></div>
     </header>

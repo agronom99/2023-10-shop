5be6519e2336d424f9291e05301a8194979d2b9b
Vitalii MV <agronom9913@gmail.com>
2023-10-10 13:27:37 +0300

    2023.10.10 start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "Стул сірий",
+          img: "chair-grey.jpeg",
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          category: "chairs",
+          price: "49.99",
+        },
+        {
+          id: 2,
+          title: "Стіл",
+          img: "table.webp",
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          category: "tables",
+          price: "149.99",
+        },
+        {
+          id: 3,
+          title: "Диван",
+          img: "sofa.jpeg",
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          category: "sofa",
+          price: "549.99",
+        },
+        {
+          id: 4,
+          title: "Лампа",
+          img: "wall-light.jpeg",
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          category: "sofa",
+          price: "24.99",
+        },
+        {
+          id: 5,
+          title: "Стілець білий",
+          img: "chair-white.jpeg",
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          category: "sofa",
+          price: "24.99",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
